17a6bea9394276839c908999c9575a9ce6cdfd38
JxxHxxx <leesin5498@naver.com>
2024-06-06 13:42:42 +0900

    SQL 결재 문서 추상화 TEST 를 위한 레코드 추가

--- src/main/resources/schema-tmp.sql
+++ src/main/resources/schema-tmp.sql
@@ -39,7 +39,11 @@ VALUES ('요청자', 'requester_name', 'vac', 'COM', '요청 정보'),
        ('품목 명', 'item_name', 'cost', 'COM','지출 내역'),
        ('지출액', 'item_price', 'cost', 'COM','지출 내역');
 
-INSERT INTO jxx_approval.jxx_confirm_document_form
+INSERT INTO jxx_confirm_document_form
 (CONFIRM_DOCUMENT_FORM_COMPANY_ID, CONFIRM_DOCUMENT_FORM_ID, CONFIRM_DOCUMENT_FORM_NAME)
 VALUES ('COM', 'vac', '휴가신청서'),
        ('COM', 'cost', '지출내역서');
+
+INSERT INTO jxx_confirm_document_content_master
+(CONTENTS)
+VALUES('{"title": "지출내역서", "item_name": "휴가신청서", "item_price": 15000}');
